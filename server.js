1a39b6ecd23c0d91cd038eb3e66dd4038ff06432
Autumn Fields <autumnF12@gmail.com>
2022-05-02 14:09:03 -0400

    syncing with sequelize and turning on server

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const routes = require('./routes');
+// import sequelize connection
+const sequelize = require('./config/connection.js');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+// sync sequelize models to the database, then turn on the server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+  console.log('');
+  console.log(`Now listening on port ${PORT}`);
+  });
+});
