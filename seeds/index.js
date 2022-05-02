decddc09215164f7eee05575de362f8ce25ed321
Autumn Fields <autumnF12@gmail.com>
2022-05-02 14:07:30 -0400

    adding seeds folder

---
+++ seeds/index.js
@@ -0,0 +1,12 @@
+
+
+const sequelize = require('../config/connection');
+
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+  console.log('\n----- DATABASE SYNCED -----\n');
+
+  process.exit(0);
+};
+
+seedAll();
