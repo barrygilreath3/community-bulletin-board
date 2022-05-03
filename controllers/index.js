f6f7a9c7a6c4b91728d636a3444e912ebc231cd9
jpugmire <jospugg@gmail.com>
2022-05-03 11:19:24 -0400

    create homepage and the route to connect it.

---
+++ controllers/index.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const home = require('./home');
+
+//write routes here
+//we can also import routes from other files. i would recommend breaking them up by feature.
+//example: 
+//if i have a file called homeRoutes.js, which contains a route to the homepage,
+//i can import that file here like so,
+//const homeRoutes = require ('./homeRoutes');
+//router.use('/', homeRoutes);
+//so, let's get the homepage
+//this adds the routes we define in the "home" file to our router
+router.use('/', home);
+
+//exporting the routes we've added to the router
+module.exports = router;
