92f4def6dac8d5cea5f3ddde2b32e904b62da1d1
jpugmire <jospugg@gmail.com>
2022-05-03 11:51:14 -0400

    change route structure to promote adding new ones!

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-const home = require('./home');
 
 //write routes here
 //we can also import routes from other files. i would recommend breaking them up by feature.
@@ -11,8 +10,7 @@ const home = require('./home');
 //router.use('/', homeRoutes);
 //so, let's get the homepage
 //this adds the routes we define in the "home" file to our router
-router.use('/', home);
-router.use('/api', apiRoutes);
+router.use('/', apiRoutes);
 
 //exporting the routes we've added to the router
 module.exports = router;
