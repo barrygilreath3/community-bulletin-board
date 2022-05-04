bb50ff7504511a86f373c5471f8a20a1b1981a6e
Barry Gilreath <barrygilreath3@gmail.com>
2022-05-04 15:48:34 -0500

    login and register modals

---
+++ public/js/register.js
@@ -0,0 +1,24 @@
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const firstName = document.querySelector('#first-name-signup').value.trim();
+  const lastName = document.querySelector('#last-name-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (firstName && lastName && email && password) {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify({ firstName, lastName, email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
