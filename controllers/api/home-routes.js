7ad4c0f6978ee32979af6565d978ad72a1d5336f
autumnlf <autumnF12@gmail.com>
2022-05-04 16:43:34 -0400

    fixed route!

--- controllers/api/home-routes.js
+++ controllers/api/home-routes.js
@@ -1,60 +1,61 @@
 const router = require('express').Router();
+const sequelize = require('../../config/connection');
 const { Bulletin_Posts, User_Accounts } = require('../../models'); //change to match models
 const authorized = require('../../utils/authorize');
 
 //dummy post data test
-const posts = [{
-    postTitle: "Test Post 1",
-    username: "jpugmire",
-    postTime: new Date().toDateString(),
-    postContent: "I saw a sign that said not to drink the fountain water so i made some tea with it and now I have an infection"
-},{
-    postTitle: "Test Post 2",
-    username: "afields",
-    postTime: new Date().toDateString()
-},{
-    postTitle: "Test Post 3",
-    username: "jatkinson",
-    postTime: new Date().toDateString()
-},{
-    postTitle: "Test Post 4",
-    username: "bgilreath",
-    postTime: new Date().toDateString()
-},];
+// const posts = [{
+//     postTitle: "Test Post 1",
+//     username: "jpugmire",
+//     postTime: new Date().toDateString(),
+//     postContent: "I saw a sign that said not to drink the fountain water so i made some tea with it and now I have an infection"
+// }, {
+//     postTitle: "Test Post 2",
+//     username: "afields",
+//     postTime: new Date().toDateString()
+// }, {
+//     postTitle: "Test Post 3",
+//     username: "jatkinson",
+//     postTime: new Date().toDateString()
+// }, {
+//     postTitle: "Test Post 4",
+//     username: "bgilreath",
+//     postTime: new Date().toDateString()
+// },];
 
 //get bulletin posts and render homepage
-// router.get('/', async (req, res) => {
-//     try {
-//         // Get all bulletin posts and JOIN with user data
-//         const postData = await Bulletin_Posts.findAll({
-//             include: [
-//                 {
-//                     model: User_Accounts,
-//                     attributes: ['username'],
-//                 },
-//             ],
-//         });
-
-//         // Serialize data so the template can read it
-//         const posts = postData.map((post) => post.get({ plain: true }));
-
-//         // Pass serialized data and session flag into template
-//         res.render('home', {
-//             posts,
-//             logged_in: req.session.logged_in
-//         });
-//     } catch (err) {}
-// });
-
-//get homepage
 router.get('/', async (req, res) => {
     try {
-        res.render('home', {posts});
-    }
-    catch (err) {
-        res.status(500).json(err);
-    }
+        // Get all bulletin posts and JOIN with user data
+        const postData = await Bulletin_Posts.findAll({
+            include: [
+                {
+                    model: User_Accounts,
+                    attributes: ['username'],
+                },
+            ],
+        });
+
+        // Serialize data so the template can read it
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        // Pass serialized data and session flag into template
+        res.render('home', {
+            posts
+            //logged_in: req.session.logged_in
+        });
+    } catch (err) { }
 });
+
+//get homepage
+// router.get('/', async (req, res) => {
+//     try {
+//         res.render('home', { posts });
+//     }
+//     catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
 
 //if we make a separate homepage for being logged in
 // router.get('/loggedIn', authorized, async (req, res) => {
