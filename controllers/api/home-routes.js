fdec64b527e028e9dc6c7ae55800bfc09c3af6cf
autumnlf <autumnF12@gmail.com>
2022-05-04 12:14:04 -0400

    api route for rendering homepage and posts

--- controllers/api/home-routes.js
+++ controllers/api/home-routes.js
@@ -1,13 +1,52 @@
 const router = require('express').Router();
+const { Bulletin_Posts, User_Accounts } = require('../../models'); //change to match models
+const authorized = require('../../utils/authorize');
 
-//get homepage
+
+//get bulletin posts and render homepage
 router.get('/', async (req, res) => {
     try {
-        res.render('home');
-    }
-    catch (err) {
+        // Get all bulletin posts and JOIN with user data
+        const postData = await Bulletin_Posts.findAll({
+            include: [
+                {
+                    model: User_Accounts,
+                    attributes: ['username'],
+                },
+            ],
+        });
+
+        // Serialize data so the template can read it
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        // Pass serialized data and session flag into template
+        res.render('home', {
+            posts,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
         res.status(500).json(err);
     }
-})
+});
+
+//if we make a separate homepage for being logged in
+// router.get('/loggedIn', authorized, async (req, res) => {
+//     try {
+//         // Find the logged in user based on the session ID
+//         const userData = await User_Accounts.findByPk(req.session.user_id, {
+//             attributes: { exclude: ['password'] },
+//             include: [{ model: Bulletin_Posts }],
+//         });
+
+//         const user = userData.get({ plain: true });
+
+//         res.render('loggedIn', {
+//             ...user,
+//             logged_in: true
+//         });
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
 
 module.exports = router;
