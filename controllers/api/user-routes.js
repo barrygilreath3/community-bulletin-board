1b54be68ef269e5cbc0cf47f69d5878adfd88250
jpugmire <jospugg@gmail.com>
2022-05-05 16:30:30 -0400

    make changes necessary to log out, as well as split off modals into their own partial files

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -55,9 +55,8 @@ router.post('/login', async (req, res) => {
 router.post('/logout', (req, res) => {
   if (req.session.logged_in) {
     req.session.destroy(() => {
-      res.status(204).end();
-
       res.json('Successfully logged out!')
+      res.status(204).end();
     });
   } else {
     res.status(404).end();
