c7f31362ab9f357b18fc9555cc3fabed8ed1e784
autumnlf <autumnF12@gmail.com>
2022-05-05 09:43:52 -0400

    fixed route path

--- public/js/login.js
+++ public/js/login.js
@@ -7,7 +7,7 @@ const loginFormHandler = async (event) => {
 
   if (username && password) {
     // Send a POST request to the API endpoint
-    const response = await fetch('/api/users/login', {
+    const response = await fetch('/user/login', {
       method: 'POST',
       body: JSON.stringify({ email, password }),
       headers: { 'Content-Type': 'application/json' },
