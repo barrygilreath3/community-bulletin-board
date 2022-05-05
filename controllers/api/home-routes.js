82e387f85c3c3832782fa594e62c010d451c9fe4
autumnlf <autumnF12@gmail.com>
2022-05-05 18:13:30 -0400

    removed old commented out lines

--- controllers/api/home-routes.js
+++ controllers/api/home-routes.js
@@ -46,24 +46,5 @@ router.get('/', async (req, res) => {
     } catch (err) { }
 });
 
-//if we make a separate homepage for being logged in
-// router.get('/loggedIn', authorized, async (req, res) => {
-//     try {
-//         // Find the logged in user based on the session ID
-//         const userData = await User_Accounts.findByPk(req.session.user_id, {
-//             attributes: { exclude: ['password'] },
-//             include: [{ model: Bulletin_Posts }],
-//         });
-
-//         const user = userData.get({ plain: true });
-
-//         res.render('loggedIn', {
-//             ...user,
-//             logged_in: true
-//         });
-//     } catch (err) {
-//         res.status(500).json(err);
-//     }
-// });
 
 module.exports = router;
